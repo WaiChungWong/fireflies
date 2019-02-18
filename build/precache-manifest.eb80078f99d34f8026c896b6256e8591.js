35ae32ba7b9f2813d1454c5ae94e327eb5c7aedc
WaiChungWong <johnwongwwc@gmail.com>
2019-02-18 11:27:25 +0800

    fix security vulnerability in dependency

---
+++ build/precache-manifest.eb80078f99d34f8026c896b6256e8591.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a05adbdf17a491a986d1f5620b4e7cde",
+    "url": "/fireflies/build/static/media/soundtrack.a05adbdf.mp3"
+  },
+  {
+    "revision": "0af2deccf8e695baf374",
+    "url": "/fireflies/build/static/js/runtime~main.0af2decc.js"
+  },
+  {
+    "revision": "f3b990276dd8d8d0437a",
+    "url": "/fireflies/build/static/js/main.f3b99027.chunk.js"
+  },
+  {
+    "revision": "720146ee4d12e3b6a2c7",
+    "url": "/fireflies/build/static/js/2.720146ee.chunk.js"
+  },
+  {
+    "revision": "f3b990276dd8d8d0437a",
+    "url": "/fireflies/build/static/css/main.c85937a2.chunk.css"
+  },
+  {
+    "revision": "eeeaba1f5570adf552adac8185a0a07b",
+    "url": "/fireflies/build/index.html"
+  }
+];
--- src/audioplayer/index.js
+++ src/audioplayer/index.js
@@ -3,6 +3,7 @@ import ClassNames from "classnames";
 import PropTypes from "prop-types";
 
 import Animator from "jw-animator";
+import * as Audio from "jw-audio";
 
 import Sound from "../resources/sound.png";
 import Mute from "../resources/mute.png";
@@ -14,50 +15,63 @@ class AudioPlayer extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { musicLoaded: false, soundOn: true };
+    this.state = { loaded: false, soundOn: true };
 
     this.toggleSound = this.toggleSound.bind(this);
   }
 
   toggleSound() {
-    const { audio } = this;
     const { soundOn } = this.state;
 
-    audio.muted = soundOn;
+    this.gain.gain.value = soundOn ? 0 : 0.2;
 
     this.setState({ soundOn: !soundOn });
   }
 
-  componentDidMount() {
-    const { audio } = this;
+  async componentDidMount() {
+    let destination = Audio.destination;
     const { animator } = this.props;
 
-    audio.volume = 0.2;
+    try {
+      this.source = await Audio.createMediaSource(Soundtrack);
+      this.gain = Audio.createGain();
+      this.source.connect(this.gain).connect(destination);
+      this.source.element.loop = true;
+      this.gain.gain.value = 0.2;
 
-    animator.onPause(() => audio.pause());
-    animator.onResume(() => audio.play());
+      animator.onPause(() => {
+        if (this.state.loaded) {
+          this.source.pause();
+        }
+      });
+      animator.onResume(() => {
+        if (this.state.loaded) {
+          this.source.resume();
+        }
+      });
+
+      let eventHandler = () => {
+        this.source.resume();
+        this.setState({ loaded: true });
+        document.removeEventListener("click", eventHandler);
+      };
+
+      document.addEventListener("click", eventHandler);
+    } catch (error) {}
   }
 
   render() {
-    const { musicLoaded, soundOn } = this.state;
+    const { loaded, soundOn } = this.state;
 
     return (
       <div id="audio-player">
-        <audio
-          ref={a => (this.audio = a)}
-          src={Soundtrack}
-          preload="auto"
-          autoPlay="true"
-          loop="true"
-          onCanPlayThrough={() => this.setState({ musicLoaded: true })}
-        />
-        {musicLoaded && (
+        {loaded && (
           <div id="audio-info" className={ClassNames({ show: soundOn })}>
             <span id="audio-title">Fireflies (Said The Sky Remix)</span>
             <span id="audio-author">Owl City</span>
           </div>
         )}
-        {musicLoaded && (
+        {loaded && (
           <img
             id="sound-toggle"
             src={soundOn ? Sound : Mute}
